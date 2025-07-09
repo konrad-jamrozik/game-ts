100b2caa03ad4f5e8e42c84f1ca9affb0ab0ed77
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-08 23:03:23 -0700

    add note to vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Note: the defineConfig should provide IntelliSense, per
+// https://vite.dev/config/#config-intellisense
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
 })
