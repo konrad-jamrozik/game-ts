bbdab84825a068163609bbe8fe4ddbf5337fd596
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-11 18:32:04 -0700

    test: uncomment button interaction tests in App component

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,14 +1,13 @@
-import { render } from '@testing-library/react'
-// import { render, screen } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('App', () => {
   it('should have a button "Add agents" and respond to click', async () => {
     render(<App />)
     // Add the button for the test (since it does not exist yet)
-    // screen.getByRole('button', { name: /add agents/i });
-    // userEvent.click(screen.getByRole('button', { name: /add agents/i }));
-    // expect(await screen.findByText(/success/i)).toBeInTheDocument();
+    screen.getByRole('button', { name: /add agents/iu })
+    await userEvent.click(screen.getByRole('button', { name: /add agents/iu }))
+    expect(await screen.findByText(/success/iu)).toBeInTheDocument()
   })
 })
