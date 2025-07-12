f3cb88a5ef71f4553806680f193681311bdc2472
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-11 17:50:17 -0700

    chore: comment out unused imports in App.test.tsx

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render } from '@testing-library/react'
+// import { render, screen } from '@testing-library/react'
+// import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('App', () => {
