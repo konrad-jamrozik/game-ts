0e5727d327639a7727010e8dbf7019e39a128c10
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-16 02:06:20 -0700

    feat: update theme configuration to use color schemes for dark and light modes

--- web/src/theme.tsx
+++ web/src/theme.tsx
@@ -2,16 +2,29 @@ import { red } from '@mui/material/colors'
 import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#556cd6',
+  colorSchemes: {
+    dark: {
+      palette: {
+        primary: {
+          main: '#556cd6',
+        },
+        // secondary: {
+        //   main: '#19857b',
+        // },
+        error: {
+          main: red.A400,
+        },
+      },
     },
-    // secondary: {
-    //   main: '#19857b',
-    // },
-    error: {
-      main: red.A400,
+    light: {
+      palette: {
+        primary: {
+          main: '#8593d3ff',
+        },
+        error: {
+          main: red.A400,
+        },
+      },
     },
   },
 })
