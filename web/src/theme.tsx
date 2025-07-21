e4308dc76dcba25cfe18b5bf78d6540781f7ae76
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-20 17:36:29 -0700

    feat: refactor theme configuration to use ColorSystemOptions and streamline component overrides

--- web/src/theme.tsx
+++ web/src/theme.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 import { red } from '@mui/material/colors'
-import { createTheme } from '@mui/material/styles'
+import { createTheme, type ColorSystemOptions } from '@mui/material/styles'
 
 // https://mui.com/material-ui/customization/theming/#typescript
 declare module '@mui/material/styles' {
@@ -16,42 +16,58 @@ declare module '@mui/material/styles' {
   }
 }
 
-const customTheme = createTheme({
-  colorSchemes: {
-    dark: {
-      palette: {
-        primary: {
-          main: '#556cd6',
-        },
-        error: {
-          main: red.A400,
-        },
-        background: {
-          default: '#212121',
-          paper: '#424242',
-          cardHeader: '#343434ff',
-          cardContents: '#343434ff',
-        },
-      },
+const colorSystemOptions: ColorSystemOptions = {
+  palette: {
+    primary: {
+      main: '#556cd6',
     },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#212121',
+      paper: '#282828ff',
+      cardHeader: '#343434ff',
+      cardContents: 'rgba(67, 67, 67, 1)',
+    },
+  },
+}
+
+let theme = createTheme({
+  colorSchemes: {
+    dark: colorSystemOptions,
+    // We set light to dark because we don't really support light, but the values
+    // must be the same for light and dark for the component overrides to work.
+    light: colorSystemOptions,
   },
 })
 
 // https://mui.com/material-ui/customization/theming/#createtheme-options-args-theme
-const customTheme2 = createTheme(customTheme, {
-  colorSchemes: {
-    dark: {
-      components: {
-        MuiCardContent: {
-          styleOverrides: {
-            root: {
-              backgroundColor: customTheme.palette.background.cardContents,
-            },
-          },
+theme = createTheme(theme, {
+  components: {
+    MuiCardHeader: {
+      styleOverrides: {
+        root: {
+          backgroundColor: theme.palette.background.cardHeader,
+          ...theme.unstable_sx({
+            paddingY: 1,
+          }),
         },
+      },
+    },
+    MuiCardContent: {
+      styleOverrides: {
+        root: {
+          backgroundColor: theme.palette.background.cardContents,
+        },
+      },
+    },
+    MuiStack: {
+      defaultProps: {
+        spacing: 2,
       },
     },
   },
 })
 
-export default customTheme2
+export default theme
