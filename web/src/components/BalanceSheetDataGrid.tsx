835e4ab2de9133d9771caf82d396f0df43d12d1f
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-20 22:51:28 -0700

    feat: refactor BalanceSheetDataGrid to use Card components for improved layout

--- web/src/components/BalanceSheetDataGrid.tsx
+++ web/src/components/BalanceSheetDataGrid.tsx
@@ -1,4 +1,7 @@
 import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardHeader from '@mui/material/CardHeader'
 import { DataGrid, type GridColDef, type GridRenderCellParams } from '@mui/x-data-grid'
 import { useAppSelector } from '../app/hooks'
 
@@ -29,23 +32,29 @@ export function BalanceSheetDataGrid(): React.JSX.Element {
       ),
     },
   ]
+  const colsMinWidth = columns.reduce((sum, col) => sum + (col.minWidth ?? 0), 0)
   return (
-    <Box display="flex" minWidth={columns.reduce((sum, col) => sum + (col.minWidth ?? 0), 0)}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        hideFooter
-        disableColumnMenu
-        disableRowSelectionOnClick
-        columnHeaderHeight={40}
-        rowHeight={30}
-        sx={(theme) => ({
-          bgcolor: theme.palette.background.default,
-          '& .MuiDataGrid-cell': { fontWeight: 100 },
-          '& .MuiDataGrid-columnHeaders': { fontWeight: 700 },
-        })}
-        aria-label="Balance Sheet"
-      />
-    </Box>
+    <Card>
+      <CardHeader title="Balance Sheet" />
+      <CardContent>
+        <Box minWidth={colsMinWidth}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            hideFooter
+            disableColumnMenu
+            disableRowSelectionOnClick
+            columnHeaderHeight={40}
+            rowHeight={30}
+            sx={(theme) => ({
+              bgcolor: theme.palette.background.default,
+              '& .MuiDataGrid-cell': { fontWeight: 100 },
+              '& .MuiDataGrid-columnHeaders': { fontWeight: 700 },
+            })}
+            aria-label="Balance Sheet"
+          />
+        </Box>
+      </CardContent>
+    </Card>
   )
 }
