9409f28906e07097e8b2b2531be8cbf8e00c73ef
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-22 03:21:17 -0700

    feat: progress on text wrapping and width computation, using min-content on container + minWidth or max-content on child

--- web/src/components/Leads.tsx
+++ web/src/components/Leads.tsx
@@ -15,18 +15,21 @@ type LeadCardProps = {
 
 function LeadCard({ title, intelCost, description, expiresIn }: LeadCardProps): React.JSX.Element {
   return (
-    <Card sx={{ width: '100%' }}>
+    <Card sx={{ width: 'min-content' }}>
       <CardContent>
-        <Typography variant="h4" component="div" gutterBottom>
-          {title}
-        </Typography>
-        <Stack direction="row" justifyContent="space-between">
-          <Typography variant="h6" color="text.secondary">
-            {intelCost} intel
+        <Stack minWidth={'300px'}>
+          {/* minWidth={'max-content'} */}
+          <Typography variant="h4" component="div" gutterBottom>
+            {title}
           </Typography>
-          <Typography variant="h6" color="text.secondary">
-            Expires in {expiresIn}
-          </Typography>
+          <Stack direction="row" justifyContent="space-between">
+            <Typography variant="h6" color="text.secondary">
+              {intelCost} intel
+            </Typography>
+            <Typography variant="h6" color="text.secondary">
+              Expires in {expiresIn}
+            </Typography>
+          </Stack>
         </Stack>
         <Typography variant="subtitle1">{description}</Typography>
       </CardContent>
@@ -36,7 +39,12 @@ function LeadCard({ title, intelCost, description, expiresIn }: LeadCardProps): 
 
 export function Leads(): React.JSX.Element {
   const cards: LeadCardProps[] = [
-    { title: 'foo', intelCost: 50, description: 'lorem ipsum', expiresIn: 3 },
+    {
+      title: 'Example quite long title indeed',
+      intelCost: 50,
+      description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+      expiresIn: 3,
+    },
     { title: 'bar', intelCost: 80, description: 'dolor sit', expiresIn: 5 },
     { title: 'baz', intelCost: 120, description: 'consectetur foobar', expiresIn: 2 },
     { title: 'qux', intelCost: 70, description: 'sed do baz qux', expiresIn: 7 },
