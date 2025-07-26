19b04576f5f7452b04abd3090cd00963cfc256e7
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-25 22:58:47 -0700

    docs: add scripts/dev.ps1

---
+++ scripts/dev.ps1
@@ -0,0 +1,9 @@
+# The dev scripts for game-ts are defined in my code-ps1 project at:
+# C:/Users/<username>/OneDrive/ps1
+# 
+# Specifically from
+# OneDrive/ps1/projects/kj-project-game-ts.psm1
+#
+# To reload the user profile modules, run 'p'
+# To view the imported commands, run 'shkj'
+# To view sources of code-ps1 logic in VS Code, run 'code-ps1'
