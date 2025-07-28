dcf6b063745c32a36704b1cf8d5d420d8e2b97b1
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-07-28 00:18:17 -0700

    feat: refactor App component to remove Container and streamline layout with Grid

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,4 +1,3 @@
-import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Stack from '@mui/material/Stack'
 import { AgentsDataGrid } from '../components/AgentsDataGrid'
@@ -15,42 +14,41 @@ import { SituationReportCard } from '../components/SituationReportCard'
 
 function App(): React.JSX.Element {
   return (
-    <Container maxWidth={false} sx={{ padding: 0 }}>
-      <Grid
-        container
-        direction="row"
-        justifyContent={'center'}
-        spacing={2}
-        padding={2}
-        bgcolor={'#30303052'}
-        flexWrap={'wrap'}
-        alignItems="stretch"
-      >
-        <Grid>
-          <Stack spacing={2} alignItems="center">
-            <GameControls />
-            <PlayerActions />
-            <EventLog />
-          </Stack>
-        </Grid>
-        <Grid>
-          <Stack spacing={2}>
-            <AgentsDataGrid />
-            <MissionCards />
-            <LeadCards />
-            <ArchivedMissionCards />
-            <ArchivedLeadCards />
-          </Stack>
-        </Grid>
-        <Grid>
-          <Stack spacing={2}>
-            <AssetsDataGrid />
-            <BalanceSheetDataGrid />
-            <SituationReportCard />
-          </Stack>
-        </Grid>
+    <Grid
+      container
+      direction="row"
+      spacing={2}
+      padding={2}
+      paddingX={1}
+      bgcolor={'#30303052'}
+      flexWrap={'wrap'}
+      justifyContent={'center'}
+      alignItems="stretch"
+    >
+      <Grid>
+        <Stack spacing={2} alignItems="center">
+          <GameControls />
+          <PlayerActions />
+          <EventLog />
+        </Stack>
       </Grid>
-    </Container>
+      <Grid>
+        <Stack spacing={2}>
+          <AgentsDataGrid />
+          <MissionCards />
+          <LeadCards />
+          <ArchivedMissionCards />
+          <ArchivedLeadCards />
+        </Stack>
+      </Grid>
+      <Grid>
+        <Stack spacing={2}>
+          <AssetsDataGrid />
+          <BalanceSheetDataGrid />
+          <SituationReportCard />
+        </Stack>
+      </Grid>
+    </Grid>
   )
 }
 
