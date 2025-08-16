4ddbedc8b1eea0f0782257430e6c8a8411f45534
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-08-15 21:25:55 -0700

    refactor: remove unused functions

--- web/src/model/CombatService.ts
+++ web/src/model/CombatService.ts
@@ -45,10 +45,10 @@ export function newRoll(skill: number, difficulty: number): Roll {
 }
 
 /**
- * Rolls a die (1-100 inclusive)
+ * Rolls a die (integer 1-100, inclusive)
  */
 export function rollDie(): number {
-  // Note: here we are OK using Math.floor, because Math.random() will never return 1, so there
+  // Note: here we are OK using Math.floor instead of my floor, because Math.random() will never return 1, so there
   // is no concern of floating point imprecision.
   const roll = Math.floor(Math.random() * 100) + 1
   return roll
@@ -65,46 +65,6 @@ export function calculateRollThreshold(skill: number, difficulty: number): [numb
 }
 
 /**
- * Determines if a roll is successful against a threshold
- */
-export function isRollSuccessful(roll: number, threshold: number): boolean {
-  return roll > threshold
-}
-
-/**
- * Calculates damage based on roll and threshold
- * Returns damage amount if roll failed, 0 if successful
- */
-export function calculateDamage(roll: number, threshold: number): number {
-  return roll < threshold ? threshold - roll : 0
-}
-
-/**
- * Performs a skill check against difficulty
- */
-// KJA actually use this function
-export function performSkillCheck(
-  skill: number,
-  difficulty: number,
-): {
-  roll: number
-  threshold: number
-  successful: boolean
-  damage?: number
-} {
-  const roll = rollDie()
-  const [threshold] = calculateRollThreshold(skill, difficulty)
-  const successful = isRollSuccessful(roll, threshold)
-  const damage = successful ? 0 : calculateDamage(roll, threshold)
-  return {
-    roll,
-    threshold,
-    successful,
-    damage,
-  }
-}
-
-/**
  * Calculates recovery time based on hit points lost
  */
 // KJA actually use this function
@@ -115,12 +75,4 @@ export function calculateRecoveryTime(hitPointsLost: number, maxHitPoints: numbe
 
   const hitPointsLostPercentage = (hitPointsLost / maxHitPoints) * 100
   return Math.ceil(hitPointsLostPercentage / 2)
-}
-
-/**
- * Determines if an agent is terminated based on hit points
- */
-// KJA actually use this function
-export function isAgentTerminated(currentHitPoints: number): boolean {
-  return currentHitPoints <= 0
 }
