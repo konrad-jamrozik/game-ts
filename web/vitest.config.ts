f95daac2e8f182577c250a32907b00c80b1d6779
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-09-01 00:51:19 -0700

    fix: test setup

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
           include: ['test/unit/*.test*'],
           environment: 'node',
           globals: true,
-          setupFiles: ['./test/utils/setupReactTests.ts'],
+          setupFiles: ['./test/utils/setupAllTests.ts'],
           testTimeout: 30_000,
         },
       },
@@ -48,7 +48,7 @@ export default defineConfig({
           include: ['test/e2e/*.test*'],
           environment: 'jsdom',
           globals: true,
-          setupFiles: './test/utils/setupAllTests.ts',
+          setupFiles: ['./test/utils/setupAllTests.ts', './test/utils/setupReactTests.ts'],
           testTimeout: 60_000, // Longer timeout for E2E tests
           server: {
             deps: {
