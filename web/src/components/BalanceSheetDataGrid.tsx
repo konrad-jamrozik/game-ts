c90f25fedc4d5adc5003f654f5d1823d68a1b60c
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-11-09 10:36:44 -0800

    simplify balance sheet data grid

--- web/src/components/BalanceSheetDataGrid.tsx
+++ web/src/components/BalanceSheetDataGrid.tsx
@@ -1,31 +1,22 @@
 import type { GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
 import { useAppSelector } from '../app/hooks'
-import { getMoneyDiff, getMoneyNewBalance, getIntelDiff } from '../lib/model/ruleset/ruleset'
+import { getMoneyNewBalance, getIntelNewBalance } from '../lib/model/ruleset/ruleset'
 import { DataGridCard } from './DataGridCard'
-import { agsV } from '../lib/model/agents/AgentsView'
 
 export type BalanceSheetRow = {
-  name: 'Money' | 'Funding' | 'Contracting' | 'Agent upkeep' | 'Hire cost' | 'Diff' | 'NewBalance' | 'Intel diff'
+  name: 'Money' | 'Projected' | 'Intel'
   value: number
 }
 
 export function BalanceSheetDataGrid(): React.JSX.Element {
   const gameState = useAppSelector((state) => state.undoable.present.gameState)
-  const agents = agsV(gameState.agents)
-  const contracting = agents.contractingIncome()
-  const agentUpkeep = agents.agentUpkeep()
-  const diff = getMoneyDiff(gameState)
-  const newBalance = getMoneyNewBalance(gameState)
-  const intelDiff = getIntelDiff(gameState)
+  const moneyProjected = getMoneyNewBalance(gameState)
+  const intelProjected = getIntelNewBalance(gameState)
   const rows = [
     { name: 'Money', id: 1, value: gameState.money },
-    { name: 'Funding', id: 2, value: gameState.funding },
-    { name: 'Contracting', id: 3, value: contracting },
-    { name: 'Agent upkeep', id: 4, value: agentUpkeep },
-    { name: 'Hire cost', id: 5, value: gameState.currentTurnTotalHireCost },
-    { name: '$ Diff', id: 6, value: diff },
-    { name: 'New $ balance', id: 7, value: newBalance },
-    { name: 'Intel diff', id: 8, value: intelDiff },
+    { name: 'Projected', id: 2, value: moneyProjected },
+    { name: 'Intel', id: 3, value: gameState.intel },
+    { name: 'Projected', id: 4, value: intelProjected },
   ]
   const columns: GridColDef[] = [
     { field: 'name', headerName: 'Item', minWidth: 120 },
