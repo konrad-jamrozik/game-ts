f40291a04c22a5561ca67cc0abc826f7a020bbd1
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-11-15 16:02:10 -0800

    Refactor DataGridCard to use ExpandableCard component

--- web/src/components/DataGridCard.tsx
+++ web/src/components/DataGridCard.tsx
@@ -1,12 +1,6 @@
-import ExpandLessIcon from '@mui/icons-material/ExpandLess'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import CardHeader from '@mui/material/CardHeader'
-import Collapse from '@mui/material/Collapse'
-import IconButton from '@mui/material/IconButton'
 import type { GridColDef, DataGridProps, GridRowModel } from '@mui/x-data-grid'
 import * as React from 'react'
+import { ExpandableCard } from './ExpandableCard'
 import { StyledDataGrid } from './StyledDataGrid'
 
 type DataGridCardProps = {
@@ -16,28 +10,9 @@ type DataGridCardProps = {
 } & Omit<DataGridProps, 'rows' | 'columns'>
 
 export function DataGridCard({ title, rows, columns, ...dataGridProps }: DataGridCardProps): React.JSX.Element {
-  const [expanded, setExpanded] = React.useState(true)
-
-  function handleExpandClick(): void {
-    setExpanded(!expanded)
-  }
-
   return (
-    <Card>
-      <CardHeader
-        avatar={
-          <IconButton onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
-            {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </IconButton>
-        }
-        title={title}
-        slotProps={{ title: { variant: 'h5' } }}
-      />
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <StyledDataGrid rows={rows} columns={columns} aria-label={title} {...dataGridProps} />
-        </CardContent>
-      </Collapse>
-    </Card>
+    <ExpandableCard title={title} defaultExpanded={true}>
+      <StyledDataGrid rows={rows} columns={columns} aria-label={title} {...dataGridProps} />
+    </ExpandableCard>
   )
 }
