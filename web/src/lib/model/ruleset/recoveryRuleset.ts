1bf4cdd2fb41a1b7c721aff7c2346e907e3a26a8
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-11-30 12:13:07 -0800

    update todo

--- web/src/lib/model/ruleset/recoveryRuleset.ts
+++ web/src/lib/model/ruleset/recoveryRuleset.ts
@@ -1,12 +1,6 @@
 import { ceil, div } from '../../utils/mathUtils'
 import { assertAboveZero, assertInteger, assertLessThan } from '../../utils/assert'
 
-// KJA add battery of tests protecting against float issues
-// Note that e.g. hitPointsLostPct = 0.8 and recoverPct = 0.02,
-// so we end up with 0.8 / 0.02 which may cause float imprecision issues,
-// and ceil will not help. Because of 0.8 / 0.02 = is 40.00000000000001,
-// then ceil will make it 41, but we want 40.
-// But we must keep ceil, as if recovery would be 3.1, we do want for it to be 4.
 export function getRecoveryTurns(hitPoints: number, damage: number, hitPointsRecoveryPct: number): number {
   assertAboveZero(damage, `damage must be above 0, got: ${damage}`)
   assertLessThan(damage, hitPoints, `damage must be less than hitPoints, got: ${damage} < ${hitPoints}`)
@@ -17,7 +11,7 @@ export function getRecoveryTurns(hitPoints: number, damage: number, hitPointsRec
   const hitPointsLostPct = Math.min(div(damage, hitPoints) * 100, 100)
   const recoveryTurns = ceil(div(hitPointsLostPct, hitPointsRecoveryPct))
 
-  // KJA add this fix once tests prove it is needed:
+  // KJA3 add this once it is needed, which will be once we support Fixed6 recovery pct
   // Round to 6 decimal places to eliminate floating point artifacts before applying ceil
   // const roundedRecoveryTurns = round6(rawRecoveryTurns)
   // return ceil(roundedRecoveryTurns)
