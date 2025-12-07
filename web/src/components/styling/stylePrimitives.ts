b30fb6ff95876934de038ec415ea45a9bfb9f872
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-12-07 15:43:22 -0800

    add combineSx

--- web/src/components/styling/stylePrimitives.ts
+++ web/src/components/styling/stylePrimitives.ts
@@ -1,6 +1,20 @@
-import type { Theme } from '@mui/material/styles'
+import type { SxProps, Theme } from '@mui/material/styles'
+import type { SystemStyleObject } from '@mui/system'
 
 export const destructiveButtonSx = {
   backgroundColor: (theme: Theme): string => theme.palette.error.dark,
   '&:hover': { backgroundColor: (theme: Theme): string => theme.palette.error.main },
+}
+
+type SxArray = readonly (boolean | SystemStyleObject<Theme> | ((theme: Theme) => SystemStyleObject<Theme>))[]
+
+function isSxArray(sx: SxProps<Theme>): sx is SxArray {
+  return Array.isArray(sx)
+}
+
+export function combineSx(sx1?: SxProps<Theme>, sx2?: SxProps<Theme>): SxProps<Theme> {
+  const resolvedSx1 = sx1 ? (isSxArray(sx1) ? [...sx1] : [sx1]) : []
+  const resolvedSx2 = sx2 ? (isSxArray(sx2) ? [...sx2] : [sx2]) : []
+  const combinedSx: SxProps<Theme> = [...resolvedSx1, ...resolvedSx2]
+  return combinedSx
 }
