0d5a31d41b7e1c02f9ec3dd170abd37d8359a6a6
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-12-13 17:44:42 -0800

    Split Troops and Officers into individual enemy type columns in OFFENSIVE_MISSIONS_DATA
    - Replace troops and officers string columns with separate number columns for each enemy type
    - Add columns: initiate, operative, handler, soldier, lieutenant, elite, commander, highCommander, cultLeader
    - Parse original string data and convert "-" values to 0

--- web/src/lib/collections/offensiveMissions.ts
+++ web/src/lib/collections/offensiveMissions.ts
@@ -1,8 +1,15 @@
 type OffensiveMissionRow = [
   name: string,
   expiresIn: number,
-  troops: string,
-  officers: string,
+  initiate: number,
+  operative: number,
+  handler: number,
+  soldier: number,
+  lieutenant: number,
+  elite: number,
+  commander: number,
+  highCommander: number,
+  cultLeader: number,
   moneyReward: number,
   fundingReward: number,
   panicReductionPct: number,
@@ -11,13 +18,13 @@ type OffensiveMissionRow = [
 
 // prettier-ignore
 export const OFFENSIVE_MISSIONS_DATA: OffensiveMissionRow[] = [
-  // Name,                         ExpIn, Troops,                      Officers,                           MoneyR, FundR, PanicR%, Suppr.
-  ['Apprehend cult member',            5, '2 In,   1 Op'                ,  '',                                  5,     0,   0.05,     '0'],
-  ['Raid cult safehouse',              8, '4 In,   4 Op'                , '1 Hn',                             100,     5,    0.1,     '1'],
-  ['Raid cult outpost',               10, '8 In,   8 Op,  2 Sl'         , '3 Hn',                             400,    10,    0.5,   '1-3'],
-  ['Raid cult training facility',     12, '30 In,  16 Op,  4 Sl'        , '6 Hn,  1 Lt',                      800,    15,      1,   '3-9'],
-  ['Raid cult logistics hub',         15, '12 In,  24 Op, 10 Sl,  2 El' , '5 Hn,  2 Lt, 1 Cm',               2000,    20,      2,  '5-15'],
-  ['Raid cult command center',        20, '- In,  20 Op, 20 Sl,  6 El'  , '4 Hn,  4 Lt, 3 Cm',               3000,    25,      5, '10-30'],
-  ['Raid cult regional stronghold',   30, '- In,   - Op, 40 Sl, 10 El'  , '- Hn,  8 Lt, 3 Cm, 1 HC',         5000,    50,     10, '15-45'],
-  ['Raid cult HQ',                    40, '- In,   - Op, 60 Sl, 20 El'  , '- Hn, 12 Lt, 6 Cm, 2 HC, 1 CL', 10_000,   100,     20,   'N/A'],
+  // Name,                         ExpIn, Init, Oper, Hndl, Sldr, Ltnt, Elit, Cmdr,  HCmd, CLdr, MoneyR, FundR,    PanicR%, Suppr.
+  ['Apprehend cult member',            5,    2,    1,    0,    0,    0,    0,    0,     0,    0,      5,     0,      0.05 ,     '0'],
+  ['Raid cult safehouse',              8,    4,    4,    1,    0,    0,    0,    0,     0,    0,    100,     5,      0.1  ,     '1'],
+  ['Raid cult outpost',               10,    8,    8,    3,    2,    0,    0,    0,     0,    0,    400,    10,      0.5  ,   '1-3'],
+  ['Raid cult training facility',     12,   30,   16,    6,    4,    1,    0,    0,     0,    0,    800,    15,      1    ,   '3-9'],
+  ['Raid cult logistics hub',         15,   12,   24,    5,   10,    2,    2,    1,     0,    0,   2000,    20,      2    ,  '5-15'],
+  ['Raid cult command center',        20,    0,   20,    4,   20,    4,    6,    3,     0,    0,   3000,    25,      5    , '10-30'],
+  ['Raid cult regional stronghold',   30,    0,    0,    0,   40,    8,   10,    3,     1,    0,   5000,    50,     10    , '15-45'],
+  ['Raid cult HQ',                    40,    0,    0,    0,   60,   12,   20,    6,     2,    1, 10_000,   100,     20    ,   'N/A'],
 ]
