4f610c80248cfc359c3ac10b9d4bc2948bcc46cc
Konrad Jamrozik <konrad-jamrozik@users.noreply.github.com>
2025-12-13 18:11:01 -0800

    Add level column to OFFENSIVE_MISSIONS_DATA
    - Add level field to OffensiveMissionRow type (after name, before expiresIn)
    - Add level values 1-8 to all offensive missions
    - Update header comment and documentation legend
    - Maintain column alignment

--- web/src/lib/collections/missionStatsTables.ts
+++ web/src/lib/collections/missionStatsTables.ts
@@ -6,6 +6,7 @@
  *
  * Legend:
  * - Name: Mission name/title.
+ * - Level: Mission level (1-8), indicating difficulty/progression tier.
  * - ExpIn: Number of turns before the mission expires.
  * - Enemy counts (Init, Oper, Hndl, etc.): Number of each enemy type present on the mission.
  * - MoneyR: Money reward for completing the mission.
@@ -19,6 +20,7 @@
 
 type OffensiveMissionRow = [
   name: string,
+  level: number,
   expiresIn: number,
   initiate: number,
   operative: number,
@@ -37,15 +39,15 @@ type OffensiveMissionRow = [
 
 // prettier-ignore
 export const OFFENSIVE_MISSIONS_DATA: OffensiveMissionRow[] = [
-  // Name,                         ExpIn, Init, Oper, Hndl, Sldr, Ltnt, Elit, Cmdr,  HCmd, CLdr, MoneyR, FundR,    PanicR%, Suppr.
-  ['Apprehend cult member',            5,    2,    1,    0,    0,    0,    0,    0,     0,    0,      5,     0,      0.05 ,     '0'],
-  ['Raid cult safehouse',              8,    4,    4,    1,    0,    0,    0,    0,     0,    0,    100,     5,      0.1  ,     '1'],
-  ['Raid cult outpost',               10,    8,    8,    3,    2,    0,    0,    0,     0,    0,    400,    10,      0.5  ,   '1-3'],
-  ['Raid cult training facility',     12,   30,   16,    6,    4,    1,    0,    0,     0,    0,    800,    15,      1    ,   '3-9'],
-  ['Raid cult logistics hub',         15,   12,   24,    5,   10,    2,    2,    1,     0,    0,   2000,    20,      2    ,  '5-15'],
-  ['Raid cult command center',        20,    0,   20,    4,   20,    4,    6,    3,     0,    0,   3000,    25,      5    , '10-30'],
-  ['Raid cult regional stronghold',   30,    0,    0,    0,   40,    8,   10,    3,     1,    0,   5000,    50,     10    , '15-45'],
-  ['Raid cult HQ',                    40,    0,    0,    0,   60,   12,   20,    6,     2,    1, 10_000,   100,     20    ,   'N/A'],
+  // Name,                         Level, ExpIn, Init, Oper, Hndl, Sldr, Ltnt, Elit, Cmdr,  HCmd, CLdr, MoneyR, FundR,    PanicR%, Suppr.
+  ['Apprehend cult member',            1,     5,    2,    1,    0,    0,    0,    0,    0,     0,    0,      5,     0,      0.05 ,     '0'],
+  ['Raid cult safehouse',              2,     8,    4,    4,    1,    0,    0,    0,    0,     0,    0,    100,     5,      0.1  ,     '1'],
+  ['Raid cult outpost',                3,    10,    8,    8,    3,    2,    0,    0,    0,     0,    0,    400,    10,      0.5  ,   '1-3'],
+  ['Raid cult training facility',      4,    12,   30,   16,    6,    4,    1,    0,    0,     0,    0,    800,    15,      1    ,   '3-9'],
+  ['Raid cult logistics hub',          5,    15,   12,   24,    5,   10,    2,    2,    1,     0,    0,   2000,    20,      2    ,  '5-15'],
+  ['Raid cult command center',         6,    20,    0,   20,    4,   20,    4,    6,    3,     0,    0,   3000,    25,      5    , '10-30'],
+  ['Raid cult regional stronghold',    7,    30,    0,    0,    0,   40,    8,   10,    3,     1,    0,   5000,    50,     10    , '15-45'],
+  ['Raid cult HQ',                     8,    40,    0,    0,    0,   60,   12,   20,    6,     2,    1, 10_000,   100,     20    ,   'N/A'],
 ]
 
 /**
